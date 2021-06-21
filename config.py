d14afc5747c0ce9f0b5c07288c5138cfaf4d5d67
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-06-22 00:00:42 +0530

    add  : config file

--- config.py
+++ config.py
@@ -1,17 +1,25 @@
 # all the model training and architecture parameters
 
+#general directories
+LOG_DIR = './logs'
+DESC_ROOT_DIR = './data/data/Desc_dataset/'  
+DESC_ENC_DIR = './data/data/Desc_dataset/style_enc.csv'
+
+
 # image parameters
-from support.notebooks.data_pipe import IMG_HEIGHT
+#from support.notebooks.data_pipe import IMG_HEIGHT
 
 
 IMG_WIDTH, IMG_HEIGHT = 128, 128
 IMAGE_SIZE = (128, 128)
 
 #desctiminator data pipeline
-DESC_ROOT_DIR = '../../../data/data/Desc_dataset/'  
-DESC_ENC_DIR = '../../../data/data/Desc_dataset/style_enc.csv'
+
 DESC_TRAIN_SIZE = 8000
 DESC_SHUFFLE_BUFFER = 1000
 DESC_BATCH_SIZE = 16
+DESC_EPOCHS = 2
+DESC_INIT_LR = 0.02
+M = 10
 
 
