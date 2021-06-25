0003e6747cfa6c11e1fd63fab6001b55bbca7cab
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-06-25 13:56:11 +0530

    update : config file

--- config.py
+++ config.py
@@ -2,9 +2,10 @@
 
 #general directories
 LOG_DIR = './logs'
+GAN_LOG_DIR = './logs/gan'
 DESC_ROOT_DIR = './data/data/Desc_dataset/'  
 DESC_ENC_DIR = './data/data/Desc_dataset/style_enc.csv'
-
+GAN_DATASET_DIR = './data/data/comp_matrics.npz'
 
 # image parameters
 #from support.notebooks.data_pipe import IMG_HEIGHT
@@ -12,6 +13,7 @@ DESC_ENC_DIR = './data/data/Desc_dataset/style_enc.csv'
 
 IMG_WIDTH, IMG_HEIGHT = 128, 128
 IMAGE_SIZE = (128, 128)
+IMAGE_SHAPE = (128, 128, 3)
 
 #desctiminator data pipeline
 
@@ -22,4 +24,7 @@ DESC_EPOCHS = 10
 DESC_INIT_LR = 0.02
 M = 1
 
+# GAN model paramters
+GAN_ALPHA = 0.1
+GAN_BETA = 0.5
 
