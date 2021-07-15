13bef7a643767003acf91f7bc54852d405f91231
nipun deelaka <46063855+nipdep@users.noreply.github.com>
2021-07-15 15:41:11 +0530

    add : dw gan model

---
+++ src/model/dwn_gan_model.py
@@ -0,0 +1,381 @@
+## this is called dead content generator wavelet convolution model
+## generator and descriminator content headers are imposed by VGG-16 model with input of (128, 128, 3)
+#%%
+import tensorflow as tf  
+import tensorflow_addons as tfa
+import tensorflow.keras.backend as K
+from tensorflow_addons.layers import InstanceNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.initializers import RandomNormal, HeUniform
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, Flatten, Dense, Conv2DTranspose, LeakyReLU, Activation, Dropout, BatchNormalization, LeakyReLU, GlobalMaxPool2D, Concatenate, ReLU, AveragePooling2D, Lambda, Reshape, Add
+from tensorflow.keras import losses
+from tensorflow.keras import metrics
+from tensorflow.python.keras.layers.pooling import GlobalAveragePooling2D
+import config 
+#%%
+# batch operation usng tensor slice
+def WaveletTransformAxisY(batch_img):
+    odd_img  = batch_img[:,0::2]
+    even_img = batch_img[:,1::2]
+    L = (odd_img + even_img) / 2.0
+    H = K.abs(odd_img - even_img)
+    return L, H
+
+def WaveletTransformAxisX(batch_img):
+    # transpose + fliplr
+    tmp_batch = K.permute_dimensions(batch_img, [0, 2, 1])[:,:,::-1]
+    _dst_L, _dst_H = WaveletTransformAxisY(tmp_batch)
+    # transpose + flipud
+    dst_L = K.permute_dimensions(_dst_L, [0, 2, 1])[:,::-1,...]
+    dst_H = K.permute_dimensions(_dst_H, [0, 2, 1])[:,::-1,...]
+    return dst_L, dst_H
+
+def Wavelet(batch_image):
+    # make channel first image
+    batch_image = K.permute_dimensions(batch_image, [0, 3, 1, 2])
+    r = batch_image[:,0]
+    g = batch_image[:,1]
+    b = batch_image[:,2]
+
+    # level 1 decomposition
+    wavelet_L, wavelet_H = WaveletTransformAxisY(r)
+    r_wavelet_LL, r_wavelet_LH = WaveletTransformAxisX(wavelet_L)
+    r_wavelet_HL, r_wavelet_HH = WaveletTransformAxisX(wavelet_H)
+
+    wavelet_L, wavelet_H = WaveletTransformAxisY(g)
+    g_wavelet_LL, g_wavelet_LH = WaveletTransformAxisX(wavelet_L)
+    g_wavelet_HL, g_wavelet_HH = WaveletTransformAxisX(wavelet_H)
+
+    wavelet_L, wavelet_H = WaveletTransformAxisY(b)
+    b_wavelet_LL, b_wavelet_LH = WaveletTransformAxisX(wavelet_L)
+    b_wavelet_HL, b_wavelet_HH = WaveletTransformAxisX(wavelet_H)
+
+    wavelet_data = [r_wavelet_LL, r_wavelet_LH, r_wavelet_HL, r_wavelet_HH, 
+                    g_wavelet_LL, g_wavelet_LH, g_wavelet_HL, g_wavelet_HH,
+                    b_wavelet_LL, b_wavelet_LH, b_wavelet_HL, b_wavelet_HH]
+    transform_batch = K.stack(wavelet_data, axis=1)
+
+    # level 2 decomposition
+    wavelet_L2, wavelet_H2 = WaveletTransformAxisY(r_wavelet_LL)
+    r_wavelet_LL2, r_wavelet_LH2 = WaveletTransformAxisX(wavelet_L2)
+    r_wavelet_HL2, r_wavelet_HH2 = WaveletTransformAxisX(wavelet_H2)
+
+    wavelet_L2, wavelet_H2 = WaveletTransformAxisY(g_wavelet_LL)
+    g_wavelet_LL2, g_wavelet_LH2 = WaveletTransformAxisX(wavelet_L2)
+    g_wavelet_HL2, g_wavelet_HH2 = WaveletTransformAxisX(wavelet_H2)
+
+    wavelet_L2, wavelet_H2 = WaveletTransformAxisY(b_wavelet_LL)
+    b_wavelet_LL2, b_wavelet_LH2 = WaveletTransformAxisX(wavelet_L2)
+    b_wavelet_HL2, b_wavelet_HH2 = WaveletTransformAxisX(wavelet_H2)
+
+
+    wavelet_data_l2 = [r_wavelet_LL2, r_wavelet_LH2, r_wavelet_HL2, r_wavelet_HH2, 
+                    g_wavelet_LL2, g_wavelet_LH2, g_wavelet_HL2, g_wavelet_HH2,
+                    b_wavelet_LL2, b_wavelet_LH2, b_wavelet_HL2, b_wavelet_HH2]
+    transform_batch_l2 = K.stack(wavelet_data_l2, axis=1)
+
+    # level 3 decomposition
+    wavelet_L3, wavelet_H3 = WaveletTransformAxisY(r_wavelet_LL2)
+    r_wavelet_LL3, r_wavelet_LH3 = WaveletTransformAxisX(wavelet_L3)
+    r_wavelet_HL3, r_wavelet_HH3 = WaveletTransformAxisX(wavelet_H3)
+
+    wavelet_L3, wavelet_H3 = WaveletTransformAxisY(g_wavelet_LL2)
+    g_wavelet_LL3, g_wavelet_LH3 = WaveletTransformAxisX(wavelet_L3)
+    g_wavelet_HL3, g_wavelet_HH3 = WaveletTransformAxisX(wavelet_H3)
+
+    wavelet_L3, wavelet_H3 = WaveletTransformAxisY(b_wavelet_LL2)
+    b_wavelet_LL3, b_wavelet_LH3 = WaveletTransformAxisX(wavelet_L3)
+    b_wavelet_HL3, b_wavelet_HH3 = WaveletTransformAxisX(wavelet_H3)
+
+    wavelet_data_l3 = [r_wavelet_LL3, r_wavelet_LH3, r_wavelet_HL3, r_wavelet_HH3, 
+                    g_wavelet_LL3, g_wavelet_LH3, g_wavelet_HL3, g_wavelet_HH3,
+                    b_wavelet_LL3, b_wavelet_LH3, b_wavelet_HL3, b_wavelet_HH3]
+    transform_batch_l3 = K.stack(wavelet_data_l3, axis=1)
+
+    # level 4 decomposition
+    wavelet_L4, wavelet_H4 = WaveletTransformAxisY(r_wavelet_LL3)
+    r_wavelet_LL4, r_wavelet_LH4 = WaveletTransformAxisX(wavelet_L4)
+    r_wavelet_HL4, r_wavelet_HH4 = WaveletTransformAxisX(wavelet_H4)
+
+    wavelet_L4, wavelet_H4 = WaveletTransformAxisY(g_wavelet_LL3)
+    g_wavelet_LL4, g_wavelet_LH4 = WaveletTransformAxisX(wavelet_L4)
+    g_wavelet_HL4, g_wavelet_HH4 = WaveletTransformAxisX(wavelet_H4)
+
+    wavelet_L3, wavelet_H3 = WaveletTransformAxisY(b_wavelet_LL3)
+    b_wavelet_LL4, b_wavelet_LH4 = WaveletTransformAxisX(wavelet_L4)
+    b_wavelet_HL4, b_wavelet_HH4 = WaveletTransformAxisX(wavelet_H4)
+
+
+    wavelet_data_l4 = [r_wavelet_LL4, r_wavelet_LH4, r_wavelet_HL4, r_wavelet_HH4, 
+                    g_wavelet_LL4, g_wavelet_LH4, g_wavelet_HL4, g_wavelet_HH4,
+                    b_wavelet_LL4, b_wavelet_LH4, b_wavelet_HL4, b_wavelet_HH4]
+    transform_batch_l4 = K.stack(wavelet_data_l4, axis=1)
+
+    # print('shape before')
+    # print(transform_batch.shape)
+    # print(transform_batch_l2.shape)
+    # print(transform_batch_l3.shape)
+    # print(transform_batch_l4.shape)
+
+    decom_level_1 = K.permute_dimensions(transform_batch, [0, 2, 3, 1])
+    decom_level_2 = K.permute_dimensions(transform_batch_l2, [0, 2, 3, 1])
+    decom_level_3 = K.permute_dimensions(transform_batch_l3, [0, 2, 3, 1])
+    decom_level_4 = K.permute_dimensions(transform_batch_l4, [0, 2, 3, 1])
+    
+    # print('shape after')
+    # print(decom_level_1.shape)
+    # print(decom_level_2.shape)
+    # print(decom_level_3.shape)
+    # print(decom_level_4.shape)
+    return [decom_level_1, decom_level_2, decom_level_3, decom_level_4]
+
+
+def Wavelet_out_shape(input_shapes):
+    # print('in to shape')
+    return [tuple([None, 112, 112, 12]), tuple([None, 56, 56, 12]), 
+            tuple([None, 28, 28, 12]), tuple([None, 14, 14, 12])]
+
+def define_mlt_desc_model(latent_size, image_shape):
+    style_image = Input(shape=image_shape, name='style_image')
+    # wavelet transform style_image
+    wavelet = Lambda(Wavelet, Wavelet_out_shape, name='wavelet')
+    input_l1, input_l2, input_l3, input_l4 = wavelet(style_image)
+    # print(input_l1)
+    # print(input_l2)
+    # print(input_l3)
+    # print(input_l4)
+
+    ## wavelet trasform style extraction head
+    # level one decomposition starts
+    conv_1 = Conv2D(64, kernel_size=(3, 3), padding='same', name='conv_1')(input_l1)
+    norm_1 = InstanceNormalization(name='norm_1')(conv_1)
+    relu_1 = Activation('relu', name='relu_1')(norm_1)
+
+    conv_1_2 = Conv2D(64, kernel_size=(3, 3), strides=(2, 2), padding='same', name='conv_1_2')(relu_1)
+    norm_1_2 = InstanceNormalization(name='norm_1_2')(conv_1_2)
+    relu_1_2 = Activation('relu', name='relu_1_2')(norm_1_2)
+
+    # level two decomposition starts
+    conv_a = Conv2D(filters=64, kernel_size=(3, 3), padding='same', name='conv_a')(input_l2)
+    norm_a = InstanceNormalization(name='norm_a')(conv_a)
+    relu_a = Activation('relu', name='relu_a')(norm_a)
+
+    # concate level one and level two decomposition
+    concate_level_2 = Concatenate()([relu_1_2, relu_a])
+    conv_2 = Conv2D(128, kernel_size=(3, 3), padding='same', name='conv_2')(concate_level_2)
+    norm_2 = InstanceNormalization(name='norm_2')(conv_2)
+    relu_2 = Activation('relu', name='relu_2')(norm_2)
+
+    conv_2_2 = Conv2D(128, kernel_size=(3, 3), strides=(2, 2), padding='same', name='conv_2_2')(relu_2)
+    norm_2_2 = InstanceNormalization(name='norm_2_2')(conv_2_2)
+    relu_2_2 = Activation('relu', name='relu_2_2')(norm_2_2)
+
+    # level three decomposition starts 
+    conv_b = Conv2D(filters=64, kernel_size=(3, 3), padding='same', name='conv_b')(input_l3)
+    norm_b = InstanceNormalization(name='norm_b')(conv_b)
+    relu_b = Activation('relu', name='relu_b')(norm_b)
+
+    conv_b_2 = Conv2D(128, kernel_size=(3, 3), padding='same', name='conv_b_2')(relu_b)
+    norm_b_2 = InstanceNormalization(name='norm_b_2')(conv_b_2)
+    relu_b_2 = Activation('relu', name='relu_b_2')(norm_b_2)
+
+    # concate level two and level three decomposition 
+    concate_level_3 = Concatenate()([relu_2_2, relu_b_2])
+    conv_3 = Conv2D(256, kernel_size=(3, 3), padding='same', name='conv_3')(concate_level_3)
+    norm_3 = InstanceNormalization(name='norm_3')(conv_3)
+    relu_3 = Activation('relu', name='relu_3')(norm_3)
+
+    conv_3_2 = Conv2D(256, kernel_size=(3, 3), strides=(2, 2), padding='same', name='conv_3_2')(relu_3)
+    norm_3_2 = InstanceNormalization(name='norm_3_2')(conv_3_2)
+    relu_3_2 = Activation('relu', name='relu_3_2')(norm_3_2)
+
+    # level four decomposition start
+    conv_c = Conv2D(64, kernel_size=(3, 3), padding='same', name='conv_c')(input_l4)
+    norm_c = InstanceNormalization(name='norm_c')(conv_c)
+    relu_c = Activation('relu', name='relu_c')(norm_c)
+
+    conv_c_2 = Conv2D(256, kernel_size=(3, 3), padding='same', name='conv_c_2')(relu_c)
+    norm_c_2 = InstanceNormalization(name='norm_c_2')(conv_c_2)
+    relu_c_2 = Activation('relu', name='relu_c_2')(norm_c_2)
+
+    conv_c_3 = Conv2D(256, kernel_size=(3, 3), padding='same', name='conv_c_3')(relu_c_2)
+    norm_c_3 = InstanceNormalization(name='norm_c_3')(conv_c_3)
+    relu_c_3 = Activation('relu', name='relu_c_3')(norm_c_3)
+
+    # concate level level three and level four decomposition
+    concate_level_4 = Concatenate()([relu_3_2, relu_c_3])
+    conv_4 = Conv2D(256, kernel_size=(3, 3), padding='same', name='conv_4')(concate_level_4)
+    norm_4 = InstanceNormalization(name='norm_4')(conv_4)
+    relu_4 = Activation('relu', name='relu_4')(norm_4)
+
+    conv_4_2 = Conv2D(256, kernel_size=(3, 3), strides=(2, 2), padding='same', name='conv_4_2')(relu_4)
+    norm_4_2 = InstanceNormalization(name='norm_4_2')(conv_4_2)
+    relu_4_2 = Activation('relu', name='relu_4_2')(norm_4_2)
+
+    conv_5_1 = Conv2D(256, kernel_size=(3, 3), padding='same', name='conv_5_1')(relu_4_2)
+    norm_5_1 = InstanceNormalization(name='norm_5_1')(conv_5_1)
+    relu_5_1 = Activation('relu', name='relu_5_1')(norm_5_1)
+
+    conv_5_2 = Conv2D(latent_size, kernel_size=(3, 3), padding='same', name='conv_5_2')(relu_5_1)
+    norm_5_2 = InstanceNormalization(name='norm_5_2')(conv_5_2)
+    relu_5_2 = Activation('relu', name='relu_5_2')(norm_5_2)
+
+    logits = GlobalMaxPool2D(name='logits')(relu_5_2)
+    model = Model(inputs=style_image, outputs=logits)
+    return model
+
+# dss_model = define_mlt_desc_model(64, (128, 128, 3))
+# tf.keras.utils.plot_model(dss_model, show_shapes=True)
+
+class StyleNet(tf.keras.Model):
+
+    def __init__(self, base_model):
+        super(StyleNet, self).__init__()
+        self._model = base_model 
+
+    @tf.function
+    def call(self, inputs):
+        ref_img, trans_img = inputs
+        with tf.name_scope("Style") as scope:
+            ft1  = self._model(ref_img)
+            #ft1 = tf.math.l2_normalize(ft1, axis=-1)
+        with tf.name_scope("Transfer") as scope:
+            ft2 = self._model(trans_img)
+            #ft2 = tf.math.l2_normalize(ft2, axis=-1)
+        return [ft1, ft2]
+    
+    @tf.function
+    def get_features(self, inputs):
+        return tf.math.l2_normalize(self._model(inputs), axis=-1)
+    
+    def get_base_model(self):
+        return self._model
+
+# ds_model = StyleNet(dss_model)
+# #
+
+#%%
+
+def define_encoder_block(layer_in, n_filters, batchnorm=True):
+    init = RandomNormal(stddev=0.02)
+    g = Conv2D(n_filters, (3, 3), strides=(2, 2), padding='same', kernel_initializer=init)(layer_in)
+    if batchnorm:
+        g =  BatchNormalization()(g, training=True)
+    g = LeakyReLU(alpha=0.2)(g)
+    return g
+
+def define_decoder_block(layer_in, skip_in, n_filters, dropout=True):
+    init = RandomNormal(stddev=0.02)
+    g = Conv2DTranspose(n_filters, (4, 4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
+    g = BatchNormalization()(g, training=True)
+    if dropout:
+        g = Dropout(0.4)(g, training=True)
+    #skip_in = tf.math.l2_normalize(skip_in)
+    g = Concatenate()([g, skip_in])
+    g = ReLU()(g)
+    return g
+
+def define_generator(style_header, latent_size, image_shape=(128, 128, 3)):
+    # init layers
+    init = RandomNormal(stddev=0.02)
+    content_image = Input(shape=image_shape, name='content_image')
+    style_image = Input(shape=image_shape, name='style_image')
+    # wavelet transform style_image
+    stl_latent_vec = style_header(style_image)
+    ## cnn base autoencoder block
+    #encoder model
+    cgb_model = tf.keras.applications.VGG16(include_top=False, input_shape=image_shape, input_tensor=content_image)
+    for layer in cgb_model.layers:
+        layer.trainable = False
+    vgg_output = cgb_model.output
+    e6 = define_encoder_block(vgg_output, 512)
+    # bottleneck layer
+    b = Conv2D(latent_size, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(e6)
+    stl_latent_mat = Reshape((1, 1, stl_latent_vec.shape[-1]))(stl_latent_vec)
+    merged = Concatenate()([b, stl_latent_mat])
+    b = ReLU()(merged)
+    #decoder model
+    #d1 = define_decoder_block(b, e7, 512)
+    g = Conv2DTranspose(512, (4, 4), strides=(2,2), padding='same', kernel_initializer=init)(b)
+    g = BatchNormalization()(g, training=True)
+    d2 = ReLU()(g)
+    #d2 = define_decoder_block(b, e6, 512)
+    d3 = define_decoder_block(d2, cgb_model.get_layer('block5_pool').output, 512)
+    d4 = define_decoder_block(d3, cgb_model.get_layer('block4_pool').output , 512, dropout=True)
+    d5 = define_decoder_block(d4, cgb_model.get_layer('block3_pool').output, 256, dropout=True)
+    d6 = define_decoder_block(d5, cgb_model.get_layer('block2_pool').output, 128, dropout=False)
+    d7 = define_decoder_block(d6, cgb_model.get_layer('block1_pool').output, 64, dropout=False)
+    #ouutput layer
+    g = Conv2DTranspose(3, (4, 4), strides=(2, 2), padding='same', kernel_initializer=init)(d7)
+    out_image = Activation('tanh')(g)
+    model = Model(inputs=[content_image, style_image], outputs=out_image, name='generator')
+    return model
+
+# g_model = define_generator(dss_model, config.GAN_LATENT_SIZE, config.IMAGE_SHAPE)
+# tf.keras.utils.plot_model(g_model, show_shapes=True)
+#%%
+
+def vgg_cnt_gen_model(image_shape):
+    cnt_gen_base_model = tf.keras.applications.VGG16(include_top=False, input_shape=image_shape)
+    for layer in cnt_gen_base_model.layers:
+        layer.trainable = False
+    vgg_output = cnt_gen_base_model.get_layer('block4_pool').output
+    model = Model(inputs=cnt_gen_base_model.input, outputs=vgg_output)
+    return model
+
+def define_cnt_descriminator(image_shape=(128, 128, 3)):
+    init = RandomNormal(stddev=0.02)
+    #content image input
+    in_cnt_image = Input(shape=image_shape)
+    #transfer image input 
+    in_tr_image = Input(shape=image_shape)
+    #concatnate image channel-wise
+    base_model = vgg_cnt_gen_model(image_shape)
+    with tf.name_scope("content") as scope:
+        vgg_cnt_output = base_model(in_cnt_image)
+    
+    with tf.name_scope("transfer") as scope:
+        vgg_tr_output = base_model(in_tr_image)
+
+    merged = Concatenate()([vgg_cnt_output, vgg_tr_output])
+    # patch output
+    d = Conv2D(1, (4, 4), strides=(2,2), padding='same', kernel_initializer=init)(merged)
+    patch_out = Activation('sigmoid')(d)
+    #define model
+    model = Model(inputs=[in_cnt_image, in_tr_image], outputs=patch_out, name='content_descriminator')
+    return model
+
+
+
+# dc_model = define_cnt_descriminator()
+# tf.keras.utils.plot_model(dc_model, show_shapes=True)
+#%%
+
+#ds_base_model = define_style_descrminator(config.DESCS_LATENT_SIZE, config.IMAGE_SHAPE)
+#ds_model = StyleNet(ds_base_model)
+#tf.keras.utils.plot_model(ds_model, show_shapes=True)
+
+
+def define_gan(g_model, dc_model, ds_model, image_shape=(128, 128, 3)):
+
+    for layer in dc_model.layers:
+        if not isinstance(layer, BatchNormalization):
+            layer.trainable = False
+
+    # input layer for GAN model
+    cnt_img = Input(shape=image_shape)
+    style_img = Input(shape=image_shape)
+    # generator model
+    gen_out = g_model([cnt_img, style_img])
+    # style descriminator model
+    dss_out, dst_out = ds_model([style_img, gen_out])
+    # content descriminator model
+    cnt_out = dc_model([cnt_img, gen_out])
+    model = Model(inputs=[cnt_img, style_img], outputs=[gen_out, dss_out, dst_out, cnt_out])
+    return model
+
+# gan_model = define_gan(g_model, dc_model, ds_model)
+# tf.keras.utils.plot_model(gan_model, show_shapes=True)
+
+# %%
